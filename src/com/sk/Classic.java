529929b6bcaeb63390fdc78d164163df56de15eb
7pranit <pranitkonde007@gmail.com>
2024-01-23 13:02:40 +0530

    My well commit

---
+++ src/com/sk/Classic.java
@@ -0,0 +1,14 @@
+package com.sk;
+
+public class Classic {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 25;
+		
+		int add = a+b;
+		
+		System.out.println("Addition is: "+add);
+	}
+}
